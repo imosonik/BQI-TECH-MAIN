877a94765e4048e6c5f61d518d3d574b9466e272
Geotechcompany <geoffreyaudia9@gmail.com>
2025-05-24 14:04:32 +0300

    Remove logo image from ClientLayout component to streamline the layout and improve code cleanliness.

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -52,13 +52,7 @@ export function ClientWrapper({ children }: { children: React.ReactNode }) {
           <JsonLd />
         </QueryClientProvider>
       </SettingsProvider>
-      <Image 
-        src="/bqilogo-light.png"
-        alt="BQI Tech Logo"
-        width={150}
-        height={50}
-        priority
-      />
+  
     </>
   )
 } 
