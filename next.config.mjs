31709120b129ce8a409d59e695bf75291da29f08
Geotechcompany <geoffreyaudia9@gmail.com>
2025-07-15 14:26:07 +0300

    Update Content Security Policy in next.config.js: Add core.service.elfsight.com to connect-src directives for enhanced security and widget integration.

--- next.config.mjs
+++ next.config.mjs
@@ -16,11 +16,11 @@ const nextConfig = {
                 value: [
                     "default-src 'self'",
                     "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/",
-                    "script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://static.elfsight.com",
+                    "script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://static.elfsight.com https://universe-static.elfsightcdn.com",
                     "style-src 'self' 'unsafe-inline'",
                     "img-src 'self' data: https: http:",
                     "frame-src 'self' https://www.google.com/recaptcha/ https://app.thinkstack.ai",
-                    "connect-src 'self' https://www.google.com/recaptcha/"
+                    "connect-src 'self' https://www.google.com/recaptcha/ https://core.service.elfsight.com"
                 ].join('; ')
             },
             {
