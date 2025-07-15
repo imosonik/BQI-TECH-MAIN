c29043ba1f061fc45289b8911bae02cc94eee985
Geotechcompany <geoffreyaudia9@gmail.com>
2025-07-15 14:38:50 +0300

    Refactor HomePage component and remove ChatbotWidget: Update HomePage to include metadata for SEO and restructure layout. Eliminate ChatbotWidget component to streamline the interface.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
+import { Metadata } from "next";
 import ClientHomePage from "@/components/ClientHomePage";
-import { ChatbotWidget } from "@/components/ChatbotWidget";
 import CookieConsentBanner from '@/components/CookieConsentBanner';
 
-export default async function HomePage() {
+export const metadata: Metadata = {
+  title: "BQI Tech - Custom Software Development & IT Solutions",
+  description: "BQI Tech offers custom software development, DevOps consultancy, and IT solutions for governments. Partner with top offshore software development experts today!",
+};
+
+export default function Home() {
   return (
-    <>
+    <main>
       <ClientHomePage />
-      <ChatbotWidget />
       <CookieConsentBanner />
-    </>
+    </main>
   );
 }
 
